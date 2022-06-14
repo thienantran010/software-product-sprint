ea6bacfa57ec89f75de12a8dd45fb2b5e6a1215c
Thienan <ttran010@bu.edu>
2022-06-14 18:52:22 +0000

    Style projects section

--- portfolio/src/main/webapp/index.js
+++ portfolio/src/main/webapp/index.js
@@ -7,8 +7,11 @@ function removeClass(element, className) {
     element.classList.remove(className);
 }
 
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
 
-window.onload = function () {
+window.onload =  async function () {
     /* header animation */
     addClass(document.querySelector("h1"), "slide-in-animation");
 
@@ -17,26 +20,45 @@ window.onload = function () {
     const bio = document.getElementsByClassName("receive bio")[0];
     const section = document.getElementsByClassName("section bio")[0];
 
+    const dotsArray = document.getElementsByClassName("dot");
+
     const options = {
         root: null,
-        threshold: 0,
+        threshold: 1,
         rootMargin: "0px"
     };
 
     const observer = new IntersectionObserver(
-        function(entries, observer) {
+         function dotAnimation(entries, observer) {
             entries.forEach(entry => {
                 if (!entry.isIntersecting) {
                     return;
                 }
                 else {
-                    setTimeout(() => {
+                    async function dotsAnim () {
+                    
+                        dotsArray[0].style.backgroundColor = "#63666A";
+                        await delay(100);
+                        dotsArray[0].style.backgroundColor = "#bbb"; //original dot color
+                        await delay(100);
+                        dotsArray[1].style.backgroundColor = "#63666A";
+                        await delay(100);
+                        dotsArray[1].style.backgroundColor = "#bbb"; //original dot color
+                        await delay(100);
+                        dotsArray[2].style.backgroundColor = "#63666A";
+                        await delay(100);
+                        dotsArray[2].style.backgroundColor = "#bbb"; //original dot color
+                        await delay(100);
+
                         removeClass(dots, "show");
                         addClass(dots, "hide");
                         removeClass(bio, "hide");
                         addClass(bio, "show");
-                    }, 2000);
+        
                 }
+
+                dotsAnim();
+            }
             }
             )
         }, options);
